c1a539280a644e790cad74628a4920cc2b708812
BeomsunGo <129032541+BeomsunGo@users.noreply.github.com>
2023-05-12 10:06:23 +0900

    Update translator_v1.py

--- translator_v1.py
+++ translator_v1.py
@@ -223,7 +223,7 @@ with col2:
             st.write(f"Input : {trytime+1}/{tot_cnt}")
             st.write(f"Input 길이 : {len(str(sliced_dict))}")
 #             st.write(sliced_trans_DB)
-            st.write(str(sliced_dict))
+#             st.write(str(sliced_dict))
             messages.append({"role": "system", "content": 'You are a translate program. Dictionary is one of the type of variables in python that contains keys and values. The beginning and end of a dictionary are represented by \'{\' and \'}\', respectively, and the key and value are connected by \':\'. Each key-value pair separated by \', \' with no other spaces or line break. Also, There are any space or line break between \"{\" and first key-value pair, \"}\" and last key-value pair respectively'})
             # messages.append({"role": "system", "content": 'Please translate sentenses and words from English to Korean. What you should translate are values in below dictionary and output type is also dictionary which has same keys with input dictionary'})
             messages.append({"role": "system", "content": f'Translate all the {org_lang} words and sentences in the dictionary below target dictionary into {tobe_lang}. What you should translate are all the sentenses and words. Output type is also dictionary which has same keys with input dictionary.'})
